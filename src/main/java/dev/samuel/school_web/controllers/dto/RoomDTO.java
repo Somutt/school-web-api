d46db638ed11778cdca42de35949f83f32aa1240
Samuel Moura <samuelmalves1@gmail.com>
2025-04-24 16:56:31 -0300

    room dto max capacity update

--- src/main/java/dev/samuel/school_web/controllers/dto/RoomDTO.java
+++ src/main/java/dev/samuel/school_web/controllers/dto/RoomDTO.java
@@ -1,13 +1,10 @@
 package dev.samuel.school_web.controllers.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record RoomDTO(
         @NotBlank @Size(max = 30, message = "Maximum name size is 30")
         String name,
-        @NotNull @Min(value = 1, message = "Minimum capacity is 1")
+        @NotNull @Min(value = 1, message = "Minimum capacity is 1") @Max(value = 1000, message = "Maximum capacity is 1000")
         Integer capacity
 ) {}
