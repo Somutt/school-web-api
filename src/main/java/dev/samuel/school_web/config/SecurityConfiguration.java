cc1c85b26ad195227cc9930dfbe4daf63da97573
Samuel Moura <samuelmalves1@gmail.com>
2025-09-24 23:42:28 -0300

    spring security roles init

--- src/main/java/dev/samuel/school_web/config/SecurityConfiguration.java
+++ src/main/java/dev/samuel/school_web/config/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package dev.samuel.school_web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,9 +23,30 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(configurer -> configurer.loginPage("/login").permitAll()) // enables login by form authorization
+                .formLogin(configurer -> configurer.loginPage("/login")) // enables login by form authorization
                 .httpBasic(Customizer.withDefaults()) // enables HTTP Basic authorization
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/professors/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/professors/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/professors/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/professors/**").hasAnyRole("USER", "ADMIN");
+                    authorize.requestMatchers("/students/**").hasAnyRole("USER", "ADMIN");
+                    authorize.anyRequest().authenticated();
+                })
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails u1 = User.builder().username("user").password(encoder.encode("123")).roles("USER").build();
+        UserDetails u2 = User.builder().username("admin").password(encoder.encode("321")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(u1, u2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }
